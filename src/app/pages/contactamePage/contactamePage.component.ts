96b15f14ea49360ff0e775aa17f8f6b1d2c5245f
Bryan España <bryanespana21550@gmail.com>
2024-05-18 15:28:41 -0600

    Contacts component

---
+++ src/app/pages/contactamePage/contactamePage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contactamePage',
+  templateUrl: './contactamePage.component.html',
+  styleUrls: ['./contactamePage.component.css']
+})
+export class ContactamePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
